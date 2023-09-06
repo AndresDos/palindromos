1a7f1190502e693fd26a9b0db8f3905c6dd8966b
AndresCvJobs <andres.dos.b@gmail.com>
2023-09-06 02:10:24 -0600

    Palindromo solution
    Posible solucion de Ejercicio 1 - palindromo

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # palindromos
  Ejercicio 1 - Palindromos
+
+Seleccione este ejercicio porque con base al desarrollo del problema y los ejemplos brindados pude plasmar una posible solucion para resolver dicho problema a comparacion del ejercicio 2 que no brindaba tanta informacion como el ejercicio 1
+
+trate de resolver el problema del ejercicio 1 comparando el primer caracter con el ultimo y determinar si algun caracter no coincidia, modificar su valor por el valor del opuesto de tal manera tener el mismo caracter en la posicion opuesta contemplando que si fuera numero inpar y el valor que recibo de cambios fuera superior a los digitos a modificar, pudiera utilizar el digito inpar para cambiar su valor de tal maner que podria cumplir con los cambios permitidos, tomando en cuenta que si el valor era inferior de los cambios permitidos a los valores a modificar para poder obtener un palindromo, returnar un -1
+
+- instalar las dependecias requeridas al clonar el repositorio
+- ejecutar dentro de la ruta npm run dev
+- entrar a la ruta en el navegador
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [n, setN] = useState(0);
+  const [k, setK] = useState(0);
+  const [s, setS] = useState("");
+
+  const handleValueNK = (e) => {
+    if (e.target.value.length >= 3) {
+      let [num1, num2] = e.target.value.split(" ");
+      if (0 < num1 < Math.pow(10, 5)) {
+        setN(num1);
+        setK(num2);
+      }
+    }
+  };
+
+  const handleValueS = (e) => {
+    if (0 < e.target.value < 9) {
+      if (e.target.value.length >= n) {
+        FunctionHighestValuePalindrome(e.target.value);
+      }
+    }
+  };
+
+  const FunctionHighestValuePalindrome = (s) => {
+    let count = 0;
+    let notSame = [];
+    while (
+      Number.isInteger(n / 2) ? count < n / 2 : count < Math.round(n / 2) + 1
+    ) {
+      if (s[count] == s[s.length - (count + 1)]) {
+      } else {
+        notSame.push(s.length - (count + 1));
+      }
+      count++;
+    }
+    if(notSame.length == k){
+      // s[notSame[x]] == s[s.length - (count + 1)]
+    }
+    if(notSame.length <= k){
+
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1>HighestValuePalindrome</h1>
+      <div className="wrapper">
+        <div className="container">
+          <input
+            className="valueNKInput"
+            placeholder="Ingresa N K"
+            maxLength={3}
+            onChange={(e) => {
+              handleValueNK(e);
+            }}
+          />
+          <input
+            className="valueSInput"
+            placeholder="Ingresa S"
+            minLength={n}
+            maxLength={n}
+            onChange={(e) => {
+              handleValueS(e);
+            }}
+          />
+        </div>
+        <div className="container">
+          <div>
+            <label className="label">n = {n}</label>
+            <label className="label"> k = {k}</label>
+          </div>
+          <label className="label">s ={s}</label>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
